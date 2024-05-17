f1bf9583212b8e8292bcb925c93de5e82497907a
YarBurArt <81087967+YarBurArt@users.noreply.github.com>
2024-05-17 17:50:50 +0500

    code clean

--- backend/repositories/mongodb.py
+++ backend/repositories/mongodb.py
@@ -1,18 +1,22 @@
+""" module for MongoDB repository and test it """
 import motor.motor_asyncio
 from pymongo.errors import DuplicateKeyError
 from bson.objectid import ObjectId
 from typing import List, Optional
 import asyncio
 
+
 class MongoDBRepository:
     """ Repository for MongoDB exactly for blog """
     def __init__(self, database_name: str, collection_name: str,
                  conn_url: str = "mongodb://localhost:27017"):
+        """ flexibility in selecting collection in specific DB  """
         self.client = motor.motor_asyncio.AsyncIOMotorClient(conn_url)
         self.db = self.client[database_name]
         self.collection = self.db[collection_name]
 
     async def create_post(self, post: dict) -> dict:
+        """ saves from json format to unstructured one """
         try:
             await self.collection.insert_one(post)
             return post
@@ -20,26 +24,31 @@ class MongoDBRepository:
             raise Exception("Post with duplicate key")
 
     async def get_all_posts(self) -> List[dict]:
+        """ gets possibly various json posts from the DB """
         posts = []
         async for document in self.collection.find():
             posts.append(document)
         return posts
 
     async def get_post_by_id(self, post_id: int) -> Optional[dict]:
+        """ gets json posts by post_id from the DB """
         document = await self.collection.find_one({"post_id": post_id})
         return document if document else None
 
     async def delete_post(self, post_id: int) -> bool:
+        """ deletes json post by post_id from the DB """
         delete_result = await self.collection.delete_one({"post_id": post_id})
         return delete_result.deleted_count == 1
     
     # TODO: update by token like in telegra.ph
     async def update_post(self, post_id: int, post_data: dict) -> Optional[dict]:
+        """ updates specific json part of post by post_id and new json from the DB """
         document = await self.collection.find_one_and_update(
             {"post_id": post_id}, {"$set": post_data})
         return document if document else None
         
     async def get_posts_by_text(self, place: str, query: str) -> List[dict]:
+        """ gets json posts by text in specific json place from the DB """
         posts = []
         async for document in self.collection.find({place: 
                 {"$regex": f".*{query}.*"}}):
@@ -47,6 +56,7 @@ class MongoDBRepository:
         return posts
     
     async def get_posts_by_category(self, category: str) -> List[dict]:
+        """ gets json posts by category from the DB """
         posts = []
         async for document in self.collection.find({"category": category}):
             posts.append(document)
@@ -54,6 +64,8 @@ class MongoDBRepository:
     
     
 async def test_main():  # TODO: rewrite tests to unit by unit 
+    """ Test main function to interact with MongoDB repository 
+        for CRUD posts and gets posts by text or category."""
     repository = MongoDBRepository("blog", "posts")  # dev
     new_post = {
         "post_id":1, # post id dev
@@ -86,5 +98,5 @@ async def test_main():  # TODO: rewrite tests to unit by unit
     
     
 if __name__ == "__main__":
-    asyncio.run(test_main())
+    asyncio.run(test_main())  # dont use it in production
     
