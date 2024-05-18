a2a4543c7aed1fd984f423d8184f371dbe5c2315
YarBurArt <yaroslav.chayk@gmail.com>
2024-05-18 14:26:28 +0500

    front sketch

---
+++ frontend/src/components/PostForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const PostForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: send form to serv
+    console.log('Отправляем форму:', { title, content, category });
+    // clean all
+    setTitle('');
+    setContent('');
+    setCategory('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="content">Content:</label>
+        <textarea
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          required></textarea>
+      </div>
+      <div>
+        <label htmlFor="category">Category:</label>
+        <input
+          type="text"
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Public </button>
+    </form>
+  );
+};
+
+export default PostForm;
