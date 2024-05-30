b16d477f8463fe766740332305ccb084243b85af
YarBurArt <81087967+YarBurArt@users.noreply.github.com>
2024-05-30 15:52:20 +0500

    add base ai

--- backend/utils/sec_analyzer.py
+++ backend/utils/sec_analyzer.py
@@ -1,2 +1,36 @@
 """ module with only analyzer by ai """
-# TODO: add api or local AI to moderate agressive posts
+# TODO: add api to moderate agressive posts
+#
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="parsawar/Profanity2.1")
+# res = pipe(["This restaurant is awesome", "This restaurant is awful"])
+# print (res)
+
+def check_count_by_model(text: str, pipe, label: str):
+    """ check text for profanity """
+    if len(text) < 96:
+        result = pipe([text])
+        print(result)       
+        if result[0]['label'] == label: #'abusive text':
+            return None
+        else:
+            return text
+    
+    ab_count = 0
+    parts_t = [text[i:i+96] for i in range(0, len(text), 96)]
+    results = pipe(parts_t)
+    print(results)
+    abusive_count = sum(1 for result in results if result['label'] == 'abusive text')
+    
+    if abusive_count / len(parts) > 0.9:
+        return None
+    return text
+
+if __name__ == "__main__":
+    pipe = pipeline("text-classification", model="parsawar/Profanity2.1")
+    check_count_by_model("This restaurant is awesome", pipe, 'abusive text')
+    pipe = pipeline('sentiment-analysis')  # TODO: change model
+    check_count_by_model("This restaurant is awful", pipe, 'POSITIVE')
+
+
