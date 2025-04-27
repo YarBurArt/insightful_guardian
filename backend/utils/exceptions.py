93fb7a4ebfadc1bdb4efae9e15888f772cc07fef
Yaroslav <yaroslav.chayk@gmail.com>
2025-04-27 14:00:44 +0500

    Update exceptions.py

--- backend/utils/exceptions.py
+++ backend/utils/exceptions.py
@@ -11,15 +11,26 @@ class BaseHTTPException(Exception):
 class InvalidInputException(BaseHTTPException):
     def __init__(self, detail: str):
         super().__init__(
-            status_code=422, error_code='InvalidInput',detail=detail)
+            status_code=422, 
+            error_code='InvalidInput', 
+            detail=detail
+        )
 
 class FileNotFoundException(BaseHTTPException):
     def __init__(self, detail: str):
-        super().__init__(status_code=404, error_code='FileNotFound', detail=detail)
+        super().__init__(
+            status_code=404, 
+            error_code='FileNotFound', 
+            detail=detail
+        )
 
 class PostNotFoundException(BaseHTTPException):
     def __init__(self, detail: str):
-        super().__init__(status_code=404, error_code='PostNotFound', detail=detail)
+        super().__init__(
+            status_code=404, 
+            error_code='PostNotFound', 
+            detail=detail
+        )
 
 # TODO: add detailed error messages, diagnostic information,
 # logging, and thorough testing
